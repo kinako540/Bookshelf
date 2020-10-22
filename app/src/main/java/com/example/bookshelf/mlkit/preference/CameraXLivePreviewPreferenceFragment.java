f7d4731292e938d8e1bd4f928173a894bb79da3a
Kage.pG <the_sketchbook12@icloud.com>
2020-10-22 22:25:02 +0900

    VisionProcessorBaseにエラーあり

---
+++ app/src/main/java/com/example/bookshelf/mlkit/preference/CameraXLivePreviewPreferenceFragment.java
@@ -0,0 +1,9 @@
+package com.example.bookshelf.mlkit.preference;
+
+/** Configures CameraX live preview demo settings. */
+public class CameraXLivePreviewPreferenceFragment extends LivePreviewPreferenceFragment {
+
+    public CameraXLivePreviewPreferenceFragment() {
+        isCameraXSetting = true;
+    }
+}
