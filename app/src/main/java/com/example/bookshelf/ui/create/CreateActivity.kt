70eb025fc4b60487c091dcdb9615913281b961a2
etthi745 <riku.jn35@gmail.com>
2020-11-05 12:50:48 +0900

    データベース登録の追加

--- app/src/main/java/com/example/bookshelf/ui/create/CreateActivity.kt
+++ app/src/main/java/com/example/bookshelf/ui/create/CreateActivity.kt
@@ -111,6 +111,7 @@ class CreateActivity : AppCompatActivity() {
                     values.put("storageLocation","")
                     values.put("saveDate", 20201010)
 
+                    
                     database.insertOrThrow("Book", null, values)
                 } catch (exception: Exception) {
                     Log.e("insertData", exception.toString())
